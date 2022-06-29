38e7970b45762c987d67e3e68608aa1eb6f9533a
mimuro-lab <mimurosyunya@gmail.com>
2022-06-30 06:48:34 +0900

    テスト　try-catch文のサンプルコード

---
+++ test/test_trycatch.php
@@ -0,0 +1,20 @@
+<?php
+
+class TestTryCatch
+{
+    function main(){
+        try {
+            //正常な処理
+             throw new Exception();
+            echo("正常な処理");
+           }catch (Exception $e) {
+            //例外が投げられた時の処理
+            echo("例外の処理");
+           }
+    }
+} 
+
+$test = new TestTryCatch();
+$test->main();
+
+?>
