2e16c8aaa835b6edbeb40fa309642b4f5bb84470
mimuro-lab <mimurosyunya@gmail.com>
2022-06-30 04:50:24 +0900

    mysqli　テーブルの作成＆取得

---
+++ test_mysql/test_mysql.php
@@ -0,0 +1,23 @@
+<?php
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli("localhost", "root", "", "test", 3306);
+
+$mysqli->query("DROP TABLE IF EXISTS test");
+$mysqli->query("CREATE TABLE test(id INT)");
+$mysqli->query("INSERT INTO test(id) VALUES (1), (2), (3)");
+
+$result = $mysqli->query("SELECT id FROM test ORDER BY id ASC");
+
+echo "Reverse order...\n";
+for ($row_no = $result->num_rows - 1; $row_no >= 0; $row_no--) {
+    $result->data_seek($row_no);
+    $row = $result->fetch_assoc();
+    echo " id = " . $row['id'] . "\n";
+}
+
+echo "Result set order...\n";
+foreach ($result as $row) {
+    echo " id = " . $row['id'] . "\n";
+}
+?>
